fed08ac38f723b686813550b9446b8991e0f81c1
Mojeedkush <mkusimo90@gmail.com>
2019-08-29 15:41:31 +0100

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+function displayContent (input) {
+    let values = document.getElementById("values");
+
+    if (input == 0) {
+        values.innerHTML += "0";
+    }
+    else if (input == 1) {
+        values.innerHTML += "1";
+    }
+    else if (input == 2) {
+        values.innerHTML += "2";
+    }
+    else if (input == 3) {
+        values.innerHTML += "3";
+    }
+    else if (input == 4) {
+        values.innerHTML += "4";
+    }
+    else if (input == 5) {
+        values.innerHTML += "5";
+    }
+    else if (input == 6) {
+        values.innerHTML += "6";
+    }
+    else if (input == 7) {
+        values.innerHTML += "7";
+    }
+    else if (input == 8) {
+        values.innerHTML += "8";
+    }
+    else if (input == 9) {
+        values.innerHTML += "9";
+    }
+    else if (input == c) {
+        values.innerHTML += "";
+    }
+    else if (input == '/') {
+        values.innerHTML += "/";
+    }
+    else if (input == '+') {
+        values.innerHTML += "+";
+    }
+    else if (input == '*') {
+        values.innerHTML += "*";
+    }
+    else if (input == '()') {
+        values.innerHTML += "()";
+    }
+    
+}
